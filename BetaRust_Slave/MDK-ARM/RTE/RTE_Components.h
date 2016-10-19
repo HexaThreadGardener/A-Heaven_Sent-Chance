c42fb0a2ace10d61a888da57d2b9acc3962972ae
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2016-10-19 12:16:47 +0800

    Right Again

---
+++ BetaRust_Slave/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'BetaRust_Slave' 
+ * Target:  'BetaRust_Slave' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
